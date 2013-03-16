1e142c2cac0067ed1b66396d1ab06cf6d6d11eea
TJ Holowaychuk <tj@vision-media.ca>
2013-03-16 12:05:21 -0700

    remove catch-all from example

--- examples/simple.js
+++ examples/simple.js
@@ -7,10 +7,6 @@ var Router = require('..');
 
 var router = new Router;
 
-router.get('*', function(path){
-  console.log('before %s', path);
-});
-
 router.get('/user/:id', function(id){
   console.log('show %s', id);
 });
