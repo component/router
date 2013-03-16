faf8d91d7d05c66e3043201959546d2943f74495
TJ Holowaychuk <tj@vision-media.ca>
2013-03-16 12:02:31 -0700

    tweak example

--- examples/teardown.js
+++ examples/teardown.js
@@ -5,25 +5,35 @@
 
 var Router = require('..');
 
+// some user view to muck with the dom
+
+function UserView() {
+
+}
+
+// render the view and inject junk into
+// the dom or whatever you need to do
+
+UserView.prototype.show = function(id){
+  this.id = id;
+  console.log('show user %s', id);
+};
+
+// perform "garbage collection" and
+// clean up after the view, unbinding
+// events etc
+
+UserView.prototype.unbind = function(){
+  console.log('clean up after %s', this.id);
+};
+
+var view = new UserView;
 var router = new Router;
 
 router
-.get('*')
-.before(function(path){
-  console.log('before %s', path);
-})
-.after(function(path){
-  console.log('after %s', path);
-})
-
-router
 .get('/user/:id')
-.before(function(id){
-  console.log('show %s', id);
-})
-.after(function(id){
-  console.log('cleanup %s', id);
-})
+.before(view.show.bind(view))
+.after(view.unbind.bind(view))
 
 router.dispatch('/user/2');
 router.dispatch('/user/5');
